76ab9eb8ffd0ae713d666aa9c0cd0197ebed2261
Bénédicte <woelffel.benedicte@yahoo.fr>
2018-11-06 15:51:15 +0100

    Quete Symfony 7

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,20 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="blog_list", defaults={"slug"="Article Sans Titre"})
+     * @Route("/blog", name="blog_list")
      */
-    public function show($slug)
+    public function showAll()
+    {
+
+
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        foreach ($categories as $category){
+            $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(['category'=> $category->getId()]);
+            foreach ($articles as $article){
+                $category->addArticle($article);
+            }
+            $resultats[] = $category->getArticles();
+        }
+        return $this->render('blog/index.html.twig', ['categories'=> $categories, 'resultats'=>$resultats]);
+    }
+
+    /**
+     * @Route("/blog/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="blog_detail", defaults={"slug"="Article Sans Titre"})
+     */
+    public function showArticle($slug)
     {
 
         $slug = str_replace('-', ' ', $slug);
         $slug = ucwords($slug);
 
-        return $this->render('blog/index.html.twig', ['slug' => $slug]);
+        return $this->render('blog/article.html.twig', ['slug' => $slug]);
     }
 }
